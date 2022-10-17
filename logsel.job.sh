6b07bc678a8d6c2926abb49f6b0ad5cd3a5b07a7
Frans Beyl <frans.beyl@gmail.com>
2022-10-17 13:09:03 +0200

    .

--- logsel.job.sh
+++ logsel.job.sh
@@ -1,3 +1,3 @@
-jobid=$(zowe zos-jobs submit local-file "logsel.job" --wfo --rff jobid --rft string);
+jobid=$(zowe zos-jobs submit data-set "z40275.jobs.cntl(logsel)" --wfo --rff jobid --rft string);
 zowe zos-jobs view spool-file-by-id $jobid 102;
 zowe zos-files download data-set "Z40275.LOGSEL.PS" -f syslog-selection.txt ;
